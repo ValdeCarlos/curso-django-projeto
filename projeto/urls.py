a7589ff5be686d7973bdeb84cdb21f0435e2577d
Valdemar Carlos <valdemarc.adao@hotmail.com>
2023-09-21 18:27:26 +0100

    mensagem

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(response):
+    return HttpResponse('Valdemar o analista')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
